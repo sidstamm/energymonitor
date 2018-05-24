c1b5370c1c8f47eeae6adcbfd091aa11879a2d60
Sid Stamm <stammsl@box.rose-hulman.edu>
2018-05-24 15:02:13 -0400

    Add importer.

--- www/db/import.py
+++ www/db/import.py
@@ -5,32 +5,61 @@ import sys
 import csv
 import time
 
+if len(sys.argv) < 4:
+    print("Usage: ./import.py <csvfile> <databasefile> <table>")
+    sys.exit(0)
 
-conn = sqlite3.connect(sys.argv[2]) or die("Can't open DB")
+conn = sqlite3.connect(sys.argv[2])
 c = conn.cursor()
 
 print("Connecting to db %s" %(sys.argv[2]))
 # Now open the CSV file
-with open(sys.argv[1], 'r') as csvfile:
-    print("importing file %s", sys.argv[1])
-    cr = csv.DictReader(csvfile)
-    for row in cr:
-        # Get the timestamp, date and time
-        tm_struct = time.strptime(row['Time'], "%a %b %d %H:%M:%S %Y")
-        # import it
-        c.execute('INSERT INTO envoy VALUES (?,?,?,?,?,?,?,?,?,?,?)',
-                  (time.mktime(tm_struct),
-                   time.strftime("%d-%m-%Y",tm_struct),
-                   time.strftime("%H:%M:%S",tm_struct),
-                   row['ProdWnow'],
-                   row['ConsWnow'],
-                   row['ProdWhToday'],
-                   row['ConsWhToday'],
-                   row['ProdWh7day'],
-                   row['ConsWh7day'],
-                   row['ProdWhLife'],
-                   row['ConsWhLife'])
-                  )
+try:
+    with open(sys.argv[1], 'r') as csvfile:
+        print("importing file %s into table %s" % (sys.argv[1], sys.argv[2]))
+        cr = csv.DictReader(csvfile)
+
+        if sys.argv[3] == "envoy":
+            for row in cr:
+                # Get the timestamp, date and time
+                tm_struct = time.strptime(row['Time'], "%a %b %d %H:%M:%S %Y")
+                # import it
+                c.execute('INSERT INTO envoy VALUES (?,?,?,?,?,?,?,?,?,?,?)',
+                          (time.mktime(tm_struct),
+                           time.strftime("%d-%m-%Y",tm_struct),
+                           time.strftime("%H:%M:%S",tm_struct),
+                           row['ProdWnow'],
+                           row['ConsWnow'],
+                           row['ProdWhToday'],
+                           row['ConsWhToday'],
+                           row['ProdWh7day'],
+                           row['ConsWh7day'],
+                           row['ProdWhLife'],
+                           row['ConsWhLife'])
+                          )
+        elif sys.argv[3] == "wx":
+            for row in cr:
+                # Get the timestamp, date and time
+                tmstr = "%s %s" %(row['Date'], row['Time'])
+                tm_struct = time.strptime(tmstr, "%Y%m%d %H%M")
+                # import it
+                c.execute('INSERT INTO wx VALUES (?,?,?,?,?,?,?,?,?,?)',
+                          (time.mktime(tm_struct),
+                           time.strftime("%d-%m-%Y",tm_struct),
+                           time.strftime("%H:%M:%S",tm_struct),
+                           row['Temp'],
+                           row['dewpt'],
+                           row['windspd'],
+                           row['winddir'],
+                           row['slp'],
+                           row['skycover'],
+                           row['skcnd'])
+                          )
+        else:
+            print("ERROR: invalid table %s", sys.argv[2])
+except FileNotFoundError as e:
+    print("Error: File not found %s" % sys.argv[2])
+    sys.exit(-1)
 
 conn.commit()
 conn.close()
---
+++ www/db/makedb.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+
+import sqlite3
+
+# Creates a database in "energymonitor.db"
+conn = sqlite3.connect("energymonitor.db") or die("Can't open DB")
+c = conn.cursor()
+
+envoytable = """
+CREATE TABLE envoy (Timestamp INTEGER,
+                    Date TEXT,
+                    Time TEXT,
+                    ProdWnow INTEGER,
+                    ConsWnow INTEGER,
+                    ProdWhToday INTEGER,
+                    ConsWhToday INTEGER,
+                    ProdWh7day INTEGER,
+                    ConsWh7day INTEGER,
+                    ProdWhLife INTEGER,
+                    ConsWhLife INTEGER);
+"""
+
+
+wxtable = """
+CREATE TABLE wx (Timestamp INTEGER,
+                 Date TEXT,
+                 Time TEXT,
+                 Temp REAL,
+                 dewpt REAL,
+                 windspd REAL,
+                 winddir INTEGER,
+                 slp REAL,
+                 skycover INTEGER,
+                 skcnd TEXT)
+"""
+
+
+c.execute(envoytable)
+c.execute(wxtable)
+             
+
+conn.commit()
+conn.close()
