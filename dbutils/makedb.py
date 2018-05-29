e5959402b7b60fb3734f27c4adba5f43844b41c1
Sid Stamm <sidstamm@gmail.com>
2018-05-29 17:13:04 -0400

    Update makedb script to take target as an argument.

--- dbutils/makedb.py
+++ dbutils/makedb.py
@@ -1,43 +1,51 @@
 #!/usr/bin/python3
 
 import sqlite3
+import sys
+
+# Get the file path
+if len(sys.argv) < 2:
+    print("Usage: ./makedb.py <databasefile>")
+    sys.exit(0)
+
 
 # Creates a database in "energymonitor.db"
-conn = sqlite3.connect("energymonitor.db") or die("Can't open DB")
-c = conn.cursor()
+print("Creating database %s" %(sys.argv[1]))
+with sqlite3.connect(sys.argv[1]) as conn:
+  c = conn.cursor()
 
-envoytable = """
-CREATE TABLE envoy (timestamp INTEGER,
-                    Date TEXT,
-                    Time TEXT,
-                    ProdWnow INTEGER,
-                    ConsWnow INTEGER,
-                    ProdWhToday INTEGER,
-                    ConsWhToday INTEGER,
-                    ProdWh7day INTEGER,
-                    ConsWh7day INTEGER,
-                    ProdWhLife INTEGER,
-                    ConsWhLife INTEGER);
-"""
-
-
-wxtable = """
-CREATE TABLE wx (timestamp INTEGER,
-                 Date TEXT,
-                 Time TEXT,
-                 Temp REAL,
-                 dewpt REAL,
-                 windspd REAL,
-                 winddir INTEGER,
-                 slp REAL,
-                 skycover INTEGER,
-                 skcnd TEXT)
-"""
+  envoytable = """
+  CREATE TABLE envoy (timestamp INTEGER,
+                      Date TEXT,
+                      Time TEXT,
+                      ProdWnow INTEGER,
+                      ConsWnow INTEGER,
+                      ProdWhToday INTEGER,
+                      ConsWhToday INTEGER,
+                      ProdWh7day INTEGER,
+                      ConsWh7day INTEGER,
+                      ProdWhLife INTEGER,
+                      ConsWhLife INTEGER);
+  """
 
 
-c.execute(envoytable)
-c.execute(wxtable)
-             
+  wxtable = """
+  CREATE TABLE wx (timestamp INTEGER,
+                   Date TEXT,
+                   Time TEXT,
+                   Temp REAL,
+                   dewpt REAL,
+                   windspd REAL,
+                   winddir INTEGER,
+                   slp REAL,
+                   skycover INTEGER,
+                   skcnd TEXT)
+  """
 
-conn.commit()
-conn.close()
+
+  c.execute(envoytable)
+  c.execute(wxtable)
+               
+
+  conn.commit()
+  conn.close()
